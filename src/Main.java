3b3f4ec3d8ccdb7c92674b29c6631db13a633d98
kumarprakash21 <37625300+kumarprakash21@users.noreply.github.com>
2026-05-02 18:52:11 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
-import java.util.Date;
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 public class Main {
-    public static void main(String[] args) {
-        Date currentDate = new Date();
-        System.out.println("Hello, Docker! Current date: " + currentDate);
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello from Docker Browser!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+        server.start();
+        System.out.println("Server started on port 8080");
     }
 }
